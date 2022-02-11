6c8961ca8cb9edd44a085f2f6b9e6dea48fd9ca7
fetusslave <67865911+fetusslave@users.noreply.github.com>
2022-02-11 19:56:58 +0800

    can smile

--- m.py
+++ m.py
@@ -1,7 +1,7 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-from math import atan, pi
+from math import atan2, pi
 
 
 mpdraw = mp.solutions.drawing_utils
@@ -14,7 +14,6 @@ height = 720
 width = 1280
 
 
-
 def getxy(landmarks):
     global width, height
     points = []
@@ -23,7 +22,7 @@ def getxy(landmarks):
     return points
 
 def draw(landmarks):
-    global width, height, w, h
+    global width, height, w
     points = getxy(landmarks)
 
     distance = (((landmarks[9].x-landmarks[10].x)**2)+((landmarks[9].y-landmarks[10].y)**2)+((abs(landmarks[9].z)-abs(landmarks[10].z))**2))**0.5
@@ -34,7 +33,6 @@ def draw(landmarks):
     legl_points = np.array([points[23], points[25], points[27], points[29], points[31]], np.int32)
     legr_points = np.array([points[24], points[26], points[28], points[30], points[32]], np.int32)
 
-
     r = int(2857*distance)
     cv2.fillPoly(w, pts=[b_points], color=(255, 0, 255))
     cv2.circle(w, tuple(points[0]), r, (255, 0, 255), -1)
@@ -42,7 +40,6 @@ def draw(landmarks):
     cv2.circle(w, tuple(points[5]), int(r/5), (0, 0, 0), -1)
     cv2.circle(w, (int(points[2][0] + (r / 10)), int(points[2][1] + (r / 10))), int(r / 20), (255, 255, 255), -1)
     cv2.circle(w, (int(points[5][0] + (r / 10)), int(points[5][1] + (r / 10))), int(r / 20), (255, 255, 255), -1)
-    #cv2.line(w, tuple(points[9]), tuple(points[10]), (0, 0, 0), 10)
     cv2.polylines(w, [arml_points], False, (0, 0, 0), 10)
     cv2.polylines(w, [armr_points], False, (0, 0, 0), 10)
     cv2.polylines(w, [legl_points], False, (0, 0, 0), 10)
@@ -58,6 +55,15 @@ def drawhand(landmarks):
             cv2.polylines(w, [finger], False, (0, 0, 0), 10)
             finger = []
 
+def midpoint(point1, point2):
+    x = int((point1[0]+point2[0])/2)
+    y = int((point1[1]+point2[1])/2)
+    return [x, y]
+
+
+def distance(point1, point2):
+    return (((point1[0] - point2[0]) ** 2) + ((point1[1] - point2[1]) ** 2)) ** 0.5
+
 
 def drawmouth(landmarks):
     points = getxy(landmarks)
@@ -65,20 +71,25 @@ def drawmouth(landmarks):
     #lip_l_o = [375, 321, 405, 314, 17, 84, 181, 91, 146, 61]
     #lip_u_i = [78, 191, 80, 81, 82, 13, 312, 311, 310, 415, 308]
     #lip_l_i = [324, 318, 402, 317, 14, 87, 178, 88, 95, 78]
+
     lip_u = points[13]
     lip_l = points[14]
     lip_left = points[308]
     lip_right = points[78]
-    center = (int((lip_left[0]+lip_right[0])/2), int((lip_left[1]+lip_right[1])/2))
-    x_axis_length = int(((((lip_left[0]-lip_right[0])**2)+((lip_left[1]-lip_right[1])**2))**0.5)/2)
-    y_axis_length = int(((((lip_u[0] - lip_l[0]) ** 2) + ((lip_u[1] - lip_l[1]) ** 2)) ** 0.5)/2)
-    #print(x_axis_length, y_axis_length)
-    if y_axis_length < 5:
-        y_axis_length = 5
-    angle = (atan(y_axis_length/x_axis_length)*180)/pi
-    if lip_left[1] < lip_right[1]:
-        angle *= -1
-    cv2.ellipse(w, center, (x_axis_length, y_axis_length), angle, 0, 360, (0, 0, 0), -1)
+
+    center = midpoint(lip_left, lip_right)
+
+    x_axis_length = int(distance(lip_left, lip_right)/2)
+    y_axis_length_l = int(distance(center, lip_l))
+    y_axis_length_u = int(distance(center, lip_u))
+
+    y_l = center[1]-lip_right[1]
+    x_l = center[0]-lip_right[0]
+
+    angle = (atan2(y_l, x_l) * 180) / pi
+
+    cv2.ellipse(w, center, (x_axis_length, y_axis_length_l), angle, 0, 180, (0, 0, 0), -1)
+    cv2.ellipse(w, center, (x_axis_length, y_axis_length_u), angle, 180, 360, (0, 0, 0), -1)
 
 
 
@@ -97,12 +108,11 @@ while cap.isOpened():
     if res.right_hand_landmarks:
         drawhand(res.right_hand_landmarks.landmark)
 
-    mpdraw.draw_landmarks(img, res.pose_landmarks, mpholistic.POSE_CONNECTIONS)
 
     if res.face_landmarks:
         drawmouth(res.face_landmarks.landmark)
 
 
-    cv2.imshow('', w)
+    cv2.imshow('aaaaa', w)
 
     cv2.waitKey(1)
